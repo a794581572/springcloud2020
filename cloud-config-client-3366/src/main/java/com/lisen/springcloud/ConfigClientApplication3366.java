ddd1a3a0688b5472cfb4536b7b6d318cf99f0e8c
谁折南枝傍小丛　　　　　 <794581572@qq.com>
2020-08-18 05:38:47 +0800

    springcloud Bus 消息总线 3366 客户端抓取3344的配置 3344 3355 3366 添加bus-amqp模块依赖 使用rabbitmq实现全局广播动态刷新和定点通知
    全局广播:curl -X POST "http://127.0.0.1:3344/actuator/bus-refresh"
    定点通知:curl -X POST "http://127.0.0.1:3344/actuator/bus-refresh/config-client:3355"

---
+++ cloud-config-client-3366/src/main/java/com/lisen/springcloud/ConfigClientApplication3366.java
@@ -0,0 +1,17 @@
+package com.lisen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lisen
+ * @create 2020/8/18 4:24
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/lisen/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.lisen.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lisen
+ * @create 2020/8/18 4:25
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+}
